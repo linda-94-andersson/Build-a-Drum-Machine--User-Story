0e0594dee4ed20f12b8c0ed6258ebe7f976a1b27
Linda Andersson <linda.29.andersson@gmail.com>
2022-12-29 13:16:05 +0100

    Added source and handle click

--- src/components/drum-pads/A.jsx
+++ src/components/drum-pads/A.jsx
@@ -1,13 +1,20 @@
 import React from "react";
 
 const A = () => {
+  const handleClick = () => {
+    const audio = document.getElementById("A");
+    audio.play();
+  };
+
   return (
-    <button id="heater-4" className="drum-pad">
-      <audio
-        id="A"
-        className="clip"
-        src="../../assets/audio/Heater-4_1.mp3"
-      ></audio>
+    <button id="heater-4" className="drum-pad" onClick={handleClick}>
+      <audio id="A" className="clip">
+        <source
+          src="https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3"
+          type="audio/mpeg"
+        />
+        <source src="../../assets/audio/Heater-4_1.mp3" type="audio/mpeg" />
+      </audio>
       A
     </button>
   );
